0e5d9378ad269ff3b2e9a609f3d6d921e9478505
Quaellezed <92826001+Quaellezed@users.noreply.github.com>
2022-02-05 19:44:40 +0100

    Update props.item.imageUrl
    Update props.item.imageUrl to get url instead of local img

--- React props page/Components/Places.js
+++ React props page/Components/Places.js
@@ -4,7 +4,7 @@ export default function Places(props) {
     
     return (
         <div className="places">
-            <img src={`images/${props.item.imageUrl}`} className="places--image" />
+            <img src={`${props.item.imageUrl}`} className="places--image" />
             <div>       
                 <img src="location-logo.png" className="places--locationlogo" />
                 <span>{props.item.googleMapsUrl}</span> <span>{props.item.title}</span>              
